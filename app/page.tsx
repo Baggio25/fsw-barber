69e68c97537a634dd253276d2fb20b80d3bd1b5e
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-12-28 22:10:50 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h2>FSW Barber</h2>
+      <Button>Novo</Button>
     </div>
   );
 }
