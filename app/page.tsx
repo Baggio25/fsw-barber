00c5edce79b83112decebbbd7ed29cb8e488d998
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-12-31 17:23:36 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "./_components/ui/avatar";
 
 export default function Home() {
   return (
@@ -27,6 +30,31 @@ export default function Home() {
             alt="Agende nos melhores com FSW Barber"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage
+                    src="https://avatars.githubusercontent.com/u/26611668?v=4"
+                    alt="RB"
+                  />
+                  <AvatarFallback>RB</AvatarFallback>
+                </Avatar>
+                <p className="text-sm">Vintage Barber</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Janeiro</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">18:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
